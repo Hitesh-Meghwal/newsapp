041c31707d12e99ce13b163e641a8743c412658c
Hitesh_Meghwal <21sycshiteshmeghwal@gmail.com>
2025-01-05 12:59:36 +0530

    apikey variable created and apikey changed

--- lib/utils/appConstant/app_constant.dart
+++ lib/utils/appConstant/app_constant.dart
@@ -1,21 +1,22 @@
 class AppConstant {
+  static const String apiKey = "a93b081cfe6341c58fb28de53bcbf179";
   // Get yesterday's date in the required format (YYYY-MM-DD)
   static String getFormattedDate() {
-    final yesterday = DateTime.now().subtract(Duration(days: 1));
+    final yesterday = DateTime.now().subtract(const Duration(days: 1));
     return "${yesterday.year}-${yesterday.month.toString().padLeft(2, '0')}-${yesterday.day.toString().padLeft(2, '0')}";
   }
 
   // Base URL for trending news with a dynamically calculated date
   static String getTrendingNews() {
     final formattedDate = getFormattedDate();
-    return "https://newsapi.org/v2/everything?q=tesla&from=$formattedDate&sortBy=publishedAt&apiKey=bce6923a79b64f8db20607d7711449fd";
+    return "https://newsapi.org/v2/everything?q=tesla&from=$formattedDate&sortBy=publishedAt&apiKey=$apiKey";
   }
 
   static const String getBreakingNews =
-      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=bce6923a79b64f8db20607d7711449fd";
+      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=$apiKey";
 
 // This method builds the URL dynamically based on the category name
   static String getCategoryNews(String categoryName) {
-    return "https://newsapi.org/v2/top-headlines?country=us&category=$categoryName&apiKey=bce6923a79b64f8db20607d7711449fd";
+    return "https://newsapi.org/v2/top-headlines?country=us&category=$categoryName&apiKey=$apiKey";
   }
 }
